d04559c1ae2c1e814afb758dd2f760b9b97ff21e
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-07 14:54:12 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -6,7 +6,7 @@ function increaseCounter(){
 }
   function increaseCounter(){
   return counter--;}
-document.addEventListener('DOMContentLoaded',function(e){
+document.addEventListener('DOMContentLoaded',(e)=>{
   
   // counter=0;
    intervalID= setTimeout(increaseCounter, 1000);
