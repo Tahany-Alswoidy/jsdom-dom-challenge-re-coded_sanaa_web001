88d7e047a02322ebff9c7e24ad5aab128c644de2
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-07 14:50:48 +0000

    Automatically backed up by Learn

---
+++ js/challenge.js
@@ -0,0 +1,18 @@
+let intervalID;
+let counter=0;
+function increaseCounter(){
+  counter++;
+  document.getElementById("counter").innerHTML=counter;
+}
+  function increaseCounter(){
+  return counter--;}
+window.addEventListener('DOMContentLoaded',function(e){
+  
+  // counter=0;
+   intervalID= setTimeout(increaseCounter, 1000);
+  // document.getElementById("counter").innerHTML=counter;
+});
+document.getElementById("plus").addEventListener('click',(event)=>{
+  event.preventDefault();
+  increaseCounter();
+})
